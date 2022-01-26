6e48277861edb4e3755e291e7e9c73cdcb56962b
Giuliano Martin Lanzani <giulilanzani@hotmail.es>
2022-01-26 14:07:02 -0300

    Schema noticias

---
+++ src/models/noticia-models.ts
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+const {Schema} = mongoose;
+
+const noticia = new Schema({
+   
+    titulo:String,
+    cuerpo:String,
+    tags:[String],
+    fecha_creacion:{ type:Date, default:Date.now},
+    fecha_edicion:{type:Date, default:Date.now},
+    fecha_baja:{type:Date, default:Date.now},
+    usuario_creador:{Type:String},
+    usuario_editor:{type:String},
+    imagen:[String],
+    organismo:{
+        dependencia: String,
+        usuario: String,
+
+    }
+
+
+})
+
+export const noticiacoleccion = mongoose.model ("noticia",noticia,"noticia")
